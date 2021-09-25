1535dcaa4e53dd89c0228eb77db78175fa2d9d8f
Bruno Teixeira <btex88@gmail.com>
2021-09-25 18:32:40 -0300

    exe-02 @ folder 01-07-01 | exe 02 done

--- 01-fundamentos/07-introducao-a-javascript-es6-e-testes-unitarios/01-javascript-es6-let-const-arrow-functions-e-template-literals/exercises-02.js
+++ 01-fundamentos/07-introducao-a-javascript-es6-e-testes-unitarios/01-javascript-es6-let-const-arrow-functions-e-template-literals/exercises-02.js
@@ -1,5 +1,23 @@
 // EXE 01
+// Crie uma função que receba um número e retorne seu fatorial.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 
 const factorial = num => num <= 1 ? 1 : num * factorial(num - 1);
 
 console.log(factorial(5));
+
+// EXE 02
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const strLength = str => {
+  const strArr = str.split(' ');
+  let longestWord = strArr[0];
+  for (let index = 0; index < strArr.length; index += 1) {
+    if (longestWord.length < strArr[index].length) {
+      longestWord = strArr[index];
+    }
+  }
+  return longestWord;
+}
+
+console.log(strLength(`test if the function gets the longest word and return it`));
