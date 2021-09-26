a9dd77c7535dfc1040304e8d9fba2a9422e5f99a
Bruno Teixeira <btex88@gmail.com>
2021-09-26 13:17:52 -0300

    010701 - part 02 - exe 04 done!

--- 01-fundamentos/07-introducao-a-javascript-es6-e-testes-unitarios/01-javascript-es6-let-const-arrow-functions-e-template-literals/exercises-02.js
+++ 01-fundamentos/07-introducao-a-javascript-es6-e-testes-unitarios/01-javascript-es6-let-const-arrow-functions-e-template-literals/exercises-02.js
@@ -21,3 +21,23 @@ const strLength = str => {
 }
 
 console.log(strLength(`test if the function gets the longest word and return it`));
+
+// EXE 04
+// Função 01 :
+  // Escreva uma função que vai receber uma string como parâmetro. 
+  // Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro.
+  // Sua função deve retornar essa nova string.
+
+const seekAndReplace = str => str.replace(/x/g, 'A');
+const strInput = 'xuxa'
+const funcOneReturn = seekAndReplace(strInput);
+console.log(funcOneReturn);
+
+// Função 02: 
+  // Escreva uma função que vai receber a string retornada da Função 01 como parâmetro. 
+  // Essa função deve concatenar as skills do array global à string que foi passada para a Função 02 via parâmetro. Você deve ordenar os skills em ordem alfabética. 
+  // Sua função deve retornar essa nova string.
+
+const arrSkills = ['javascritp', 'python', 'react']
+const concatArrays = str => {arrSkills.push(str); return arrSkills.sort().join(` `);}
+console.log(concatArrays(funcOneReturn));
