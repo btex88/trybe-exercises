70b2314fb7a28d9ee3fe133bbe41c7ee03fec221
Bruno Teixeira <btex88@gmail.com>
2021-12-04 16:21:23 -0300

    style(h1): add line break at title

--- 02-front-end/15-gerenciamento-de-estado-com-redux/01-intro/exercise-01/src/components/Card.jsx
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/01-intro/exercise-01/src/components/Card.jsx
@@ -6,21 +6,30 @@ import styles from '../styles/Card.module.css';
 const Card = ({ colors, index, previous, next }) => {
   return (
     <div className={styles.container}>
-      <h1 style={{
-        backgroundColor: colors[index],
-        color: colors[index-1]
-        }}>Color: {colors[index]}</h1>
+      <h1
+        style={{
+          backgroundColor: colors[index],
+          color: colors[index - 1],
+        }}
+      >
+        Color:
+        <br />
+        <span>{colors[index]}</span>
+      </h1>
       <Button clickFn={previous} name="Previous color" />
       <Button clickFn={next} name="Next color" />
     </div>
   );
 };
 
-const mapStateProps = ({ changeColor: {colors, index }}) => ({ colors, index });
+const mapStateProps = ({ changeColor: { colors, index } }) => ({
+  colors,
+  index,
+});
 
 const mapDispatchProps = (dispatch) => ({
-  previous: () => dispatch({ type: 'PREVIOUS_COLOR'}),
-  next: () => dispatch({ type: 'NEXT_COLOR'}),
-})
+  previous: () => dispatch({ type: 'PREVIOUS_COLOR' }),
+  next: () => dispatch({ type: 'NEXT_COLOR' }),
+});
 
 export default connect(mapStateProps, mapDispatchProps)(Card);
